425a03ab4e5d7b0f763924d7bc6d6ef82622e7c6
Eugeny Burovnikov <eugeny.burovnikov@vk.team>
2024-05-16 22:12:44 +0300

    Fix import

--- server/src/main/java/ru/practicum/shareit/item/dto/ItemDtoServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/item/dto/ItemDtoServiceImpl.java
@@ -25,7 +25,6 @@ import ru.practicum.shareit.user.model.User;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
